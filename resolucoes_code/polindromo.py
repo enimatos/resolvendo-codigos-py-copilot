cc05ef1c529c618d7d8b634eb84044098f128973
Elaine Matos <69444237+enimatos@users.noreply.github.com>
2025-06-20 22:45:57 -0300

    subindo exercício de polindromo

---
+++ resolucoes_code/polindromo.py
@@ -0,0 +1,8 @@
+# Vamos testar se uma palavra é um palíndromo?!
+
+palavra = input("Digite uma palavra: ")
+
+if palavra == palavra[::-1]:
+    print("É um palíndromo!")
+else:
+    print("Não é um palíndromo.")
