2c60801bedaee8069d207e7be7843910ba2378b2
Elaine Matos <69444237+enimatos@users.noreply.github.com>
2025-06-20 22:43:06 -0300

    subindo exercício de repetição de string

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,8 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Agora vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string repetida o número de vezes informado. 
+
+texto = input("Digite um texto: ")
+numero = int(input("Digite a quantidade de vocês que seu texto deve ser repetido: "))
+
+while numero > 0:
+    print(texto)
+    numero -= 1
