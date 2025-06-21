11ba2a65c5a0373e07d263b596e3c63ffcd20e5a
Elaine Matos <69444237+enimatos@users.noreply.github.com>
2025-06-20 22:41:16 -0300

    subindo exercicio de concatenação de string

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+
+print(f"O resultado da concatenação das strings é {dado1} {dado2} ")
